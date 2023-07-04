6c1e9dbb4931789b18f52cb422489abe0531a45c
aitkenbug <vicente.aitken@ug.uchile.cl>
2023-07-04 18:00:37 -0400

    Fixed Programs

--- echo/echo.py
+++ echo/echo.py
@@ -2,6 +2,9 @@ import serial
 import time 
 import codecs
 
+serial_port = '/dev/ttyUSB0'
+
+freq = 915
 baudrate = 9600
 mod = "SF9"
 band_width = 125
@@ -9,36 +12,36 @@ tx_pr = 8
 rx_pr = 8
 power = 22
 
+time.monotonic()
+
+ser = serial.Serial(serial_port, baudrate)
+
 def main():
     print('Welcome to Echo.py script for lora echo communication...\n\n')
-    port = input('Set your Serial comm port: ')
-    freq = input('Set your center frequency: ')
-    initialize_radio(port,freq)
+    initialize_radio()
     listening_ping()
 
-def listening_ping()
+def listening_ping():
     ser.write("AT+TEST=RXLRPKT".encode())
     while True:
         if ser.inWaiting():
             rx_msg = ser.readline().decode()
             if '+TEST: RX ' in rx_msg:
                 msg_data = rx_msg.split('\"')[-2]
-                if hex_to_chr(msg_data) == 'ping'
+                if hex_to_chr(msg_data) == 'ping':
                     print('Ping received\nSending Echo')
                     time.sleep(1)
                     send_msg(chr_to_hex('ping'))
                     break
     listening_ping()
 
-def initialize_radio(serial_port, freq):
-    ser = serial.Serial(serial_port, baudrate)
-    ser.write("AT+MODE=TEST")
-    ser.write("AT""AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power))
+def initialize_radio():
+    ser.write("AT+MODE=TEST\n".encode())
+    ser.write("AT""AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power).encode())
     print('Radio Initialized ...')
 
 def send_msg(message):
     ser.write("AT+TEST=TXLRPKT,\"{}\"\n".format(message).encode())
-    print(ser.readline().decode())
 
 def chr_to_hex(string):
     return codecs.encode(string.encode(),'hex').decode()
--- echo/ping.py
+++ echo/ping.py
@@ -2,6 +2,8 @@ import serial
 import time 
 import codecs
 
+freq = 915
+serial_port = 'COM24'
 baudrate = 9600
 mod = "SF9"
 band_width = 125
@@ -9,41 +11,40 @@ tx_pr = 8
 rx_pr = 8
 power = 22
 
+time.monotonic()
+
+ser = serial.Serial(serial_port, baudrate) 
+
 def main():
     print('Welcome to Ping.py script for lora echo communication...\n\n')
-    port = input('Set your Serial comm port: ')
-    freq = input('Set your center frequency: ')
-    initialize_radio(port,freq)
+    initialize_radio()
     print('Timeout in 10 seconds')
     send_ping()
 
-def send_ping()
+def send_ping():
     send_msg(chr_to_hex('ping'))
     print('Ping Sent')
     t_end = time.time() + 10
     while time.time() < t_end:
-       ser.write("AT+TEST=RXLRPKT".encode())
-        while True:
-            if ser.inWaiting():
-                rx_msg = ser.readline().decode()
-                if '+TEST: RX ' in rx_msg:
-                    msg_data = rx_msg.split('\"')[-2]
-                    if hex_to_chr(msg_data) == 'ping'
+        ser.write("AT+TEST=RXLRPKT\n".encode())
+        if ser.inWaiting():
+            rx_msg = ser.readline().decode()
+            if '+TEST: RX ' in rx_msg:
+                msg_data = rx_msg.split('\"')[-2]
+                if hex_to_chr(msg_data) == 'ping':
                     print('Echo received')
                     break
-    if time.time()>t_end():
+    if time.time()>t_end:
         print('timeout, echo not received\nSending ping again...')
     send_ping()
 
-def initialize_radio(serial_port, freq):
-    ser = serial.Serial(serial_port, baudrate)
-    ser.write("AT+MODE=TEST")
-    ser.write("AT""AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power))
+def initialize_radio():
+    ser.write("AT+MODE=TEST\n".encode())
+    ser.write("AT""AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power).encode())
     print('Radio Initialized ...')
 
 def send_msg(message):
     ser.write("AT+TEST=TXLRPKT,\"{}\"\n".format(message).encode())
-    print(ser.readline().decode())
 
 def chr_to_hex(string):
     return codecs.encode(string.encode(),'hex').decode()
@@ -51,3 +52,4 @@ def chr_to_hex(string):
 def hex_to_chr(string):
     return codecs.decode(string, 'hex').decode()
 
+main()
