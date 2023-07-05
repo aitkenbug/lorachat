106c59bcb1d2ef6f3eefcf456e28dec714cf9b4c
aitkenbug <vicente.aitken@ug.uchile.cl>
2023-07-05 15:45:03 -0400

    working ping

--- echo/ping.py
+++ echo/ping.py
@@ -1,6 +1,8 @@
 import serial 
 import time 
 import codecs
+import colorama
+from colorama import Fore
 
 freq = 915
 serial_port = 'COM24'
@@ -18,21 +20,24 @@ ser = serial.Serial(serial_port, baudrate)
 def main():
     print('Welcome to Ping.py script for lora echo communication...\n\n')
     initialize_radio()
+    time.sleep(0.5)
     print('Timeout in 10 seconds')
     send_ping()
 
 def send_ping():
     send_msg(chr_to_hex('ping'))
+    time.sleep(0.5)
     print('Ping Sent')
-    t_end = time.time() + 10
+    t_end = time.time() + 9
+    ser.write("AT+TEST=RXLRPKT\n".encode())
     while time.time() < t_end:
-        ser.write("AT+TEST=RXLRPKT\n".encode())
         if ser.inWaiting():
             rx_msg = ser.readline().decode()
             if '+TEST: RX ' in rx_msg:
                 msg_data = rx_msg.split('\"')[-2]
                 if hex_to_chr(msg_data) == 'ping':
                     print('Echo received')
+                    time.sleep(0.5)
                     break
     if time.time()>t_end:
         print('timeout, echo not received\nSending ping again...')
@@ -40,7 +45,8 @@ def send_ping():
 
 def initialize_radio():
     ser.write("AT+MODE=TEST\n".encode())
-    ser.write("AT""AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power).encode())
+    time.sleep(0.5)
+    ser.write("AT+TEST=RFCFG,{},{},{},{},{},{},OFF,OFF,OFF\n".format(freq, mod, band_width, tx_pr, rx_pr, power).encode())
     print('Radio Initialized ...')
 
 def send_msg(message):
